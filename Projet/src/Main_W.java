0237bb2a112210c324c6458d3523a302d01041be
Brendan Le Lain <brendanlelain@hotmail.com>
2015-11-26 20:00:45 +0100

    add disconnect in menu

--- Projet/src/Main_W.java
+++ Projet/src/Main_W.java
@@ -1,4 +1,8 @@
 
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -11,6 +15,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.swing.JDialog;
 import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
@@ -31,6 +36,8 @@ public class Main_W extends javax.swing.JFrame {
      */
     public Main_W() {
         initComponents();
+        MyClientTab = new ClientTab();
+        jTabbedPane1.addTab("Clients",MyClientTab);
     }
 
     
@@ -45,57 +52,6 @@ public class Main_W extends javax.swing.JFrame {
     private void initComponents() {
 
         jTabbedPane1 = new javax.swing.JTabbedPane();
-        jPanel1 = new javax.swing.JPanel();
-        jTextField1 = new javax.swing.JTextField();
-        jLabel1 = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
-        jButton3 = new javax.swing.JButton();
-        jSeparator5 = new javax.swing.JSeparator();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        Connection con = null;
-        Statement st = null;
-        ResultSet rs = null;
-
-        String url = "jdbc:mysql://localhost:3306/bdd_appli";
-        String user = "admin";
-        String password = "Brendan";
-        String col[]={
-            "id", "street", "city", "zip code", "pays"
-        };
-        String data[][]=new String[10][5];
-        try
-        {
-            con = DriverManager.getConnection(url, user, password);   //establish a connection to the database, using the connection URL, user name, and password
-            st = con.createStatement();   //creates a Statement object for sending SQL statements to the database
-            rs = st.executeQuery("SELECT * FROM Adresses");  //The createStatement() method of the connection object executes
-            //the given SQL statement, which returns a single ResultSet object. The ResultSet is a table of data returned by a specific SQL statement.
-
-            int i=0;
-            while (rs.next())
-            {
-                int id = rs.getInt("ad_id");
-                String street = rs.getString("street");
-                String city=rs.getString("city");
-                String zip=rs.getString("zip_code");
-                String pays=rs.getString("country");
-                data[i][0]=id + "";
-                data[i][1]=street;
-                data[i][2]=city;
-                data[i][3]=zip;
-                data[i][4]=pays;
-                i++;
-            }
-        }
-        catch (SQLException ex)
-        {
-            System.out.println("Connection error");
-            Logger lgr = Logger.getLogger(SignIn_W.class.getName());
-            lgr.log(Level.SEVERE, ex.getMessage(), ex);     //In case of an exception, we log the error message
-
-        }
-        DefaultTableModel model = new DefaultTableModel(data,col);
-        jTable1 = new JTable(model);
         jPanel2 = new javax.swing.JPanel();
         jLabel2 = new javax.swing.JLabel();
         jTextField2 = new javax.swing.JTextField();
@@ -127,6 +83,7 @@ public class Main_W extends javax.swing.JFrame {
         menuBar = new javax.swing.JMenuBar();
         fileMenu = new javax.swing.JMenu();
         exitMenuItem = new javax.swing.JMenuItem();
+        MenuItemDisconnect = new javax.swing.JMenuItem();
         editMenu = new javax.swing.JMenu();
         jMenuItem1 = new javax.swing.JMenuItem();
         helpMenu = new javax.swing.JMenu();
@@ -135,60 +92,6 @@ public class Main_W extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jTabbedPane1.setTabPlacement(javax.swing.JTabbedPane.LEFT);
-
-        jTextField1.setCursor(new java.awt.Cursor(java.awt.Cursor.TEXT_CURSOR));
-
-        jLabel1.setText("Search");
-
-        jButton1.setText("Add");
-
-        jButton2.setText("Modify");
-
-        jButton3.setText("Remove");
-
-        jScrollPane1.setViewportView(jTable1);
-
-        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
-        jPanel1.setLayout(jPanel1Layout);
-        jPanel1Layout.setHorizontalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jButton3)
-                .addGap(90, 90, 90)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, 200, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(306, Short.MAX_VALUE))
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addGap(20, 20, 20)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jSeparator5)
-                    .addComponent(jScrollPane1))
-                .addContainerGap())
-        );
-        jPanel1Layout.setVerticalGroup(
-            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(jPanel1Layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)
-                    .addComponent(jButton3)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jSeparator5, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1)
-                .addContainerGap())
-        );
-
-        jTabbedPane1.addTab("Clients", jPanel1);
 
         jLabel2.setText("Search");
 
@@ -392,6 +295,14 @@ public class Main_W extends javax.swing.JFrame {
         });
         fileMenu.add(exitMenuItem);
 
+        MenuItemDisconnect.setText("Sign out");
+        MenuItemDisconnect.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                MenuItemDisconnectActionPerformed(evt);
+            }
+        });
+        fileMenu.add(MenuItemDisconnect);
+
         menuBar.add(fileMenu);
 
         editMenu.setMnemonic('e');
@@ -435,48 +346,53 @@ public class Main_W extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextField4ActionPerformed
 
+    private void MenuItemDisconnectActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_MenuItemDisconnectActionPerformed
+        Object[] options = {"Yes","No"};
+                
+        int n = JOptionPane.showOptionDialog(this,"Are you sure you want to sign out?","Disconnecting",
+                JOptionPane.YES_NO_OPTION,JOptionPane.QUESTION_MESSAGE,null,options,options[0]);
+        if (n==JOptionPane.YES_OPTION) {
+            this.dispose();  //we close the main window
+            SignIn_W SignW = new SignIn_W();    //and we create a new connection window
+            SignW.setLocationRelativeTo(null);
+            SignW.setVisible(true);
+        }
+    }//GEN-LAST:event_MenuItemDisconnectActionPerformed
 
+    private ClientTab MyClientTab;
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JMenuItem MenuItemDisconnect;
     private javax.swing.JMenuItem aboutMenuItem;
     private javax.swing.JMenu editMenu;
     private javax.swing.JMenuItem exitMenuItem;
     private javax.swing.JMenu fileMenu;
     private javax.swing.JMenu helpMenu;
-    private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton10;
     private javax.swing.JButton jButton11;
     private javax.swing.JButton jButton12;
     private javax.swing.JButton jButton13;
     private javax.swing.JButton jButton14;
     private javax.swing.JButton jButton15;
-    private javax.swing.JButton jButton2;
-    private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     private javax.swing.JButton jButton6;
     private javax.swing.JButton jButton7;
     private javax.swing.JButton jButton8;
     private javax.swing.JButton jButton9;
-    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JMenuItem jMenuItem1;
-    private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
     private javax.swing.JPanel jPanel5;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSeparator jSeparator1;
     private javax.swing.JSeparator jSeparator2;
     private javax.swing.JSeparator jSeparator3;
     private javax.swing.JSeparator jSeparator4;
-    private javax.swing.JSeparator jSeparator5;
     private javax.swing.JTabbedPane jTabbedPane1;
-    private javax.swing.JTable jTable1;
-    private javax.swing.JTextField jTextField1;
     private javax.swing.JTextField jTextField2;
     private javax.swing.JTextField jTextField3;
     private javax.swing.JTextField jTextField4;
